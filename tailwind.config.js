838aebe7c06e88a9196dd1b15b7b357e168f2121
Huuhoai2002 <huuhoai0511@gmail.com>
2022-08-25 14:21:12 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#E6F0FF",
+        primary: "#000336",
+        secondary: "#5A6473",
+        heading: "#101010",
+        subheading: "#7D8589",
+        blue: "#0689FF",
+      },
+    },
+  },
+  plugins: [],
+};
